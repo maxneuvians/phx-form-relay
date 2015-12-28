1889cab10edf1af2305535e68f75662eac1c2685
Max Neuvians <max@neuvians.net>
2015-12-28 10:32:45 -0500

    Refactored try/catch in sender controller

--- web/controllers/send_controller.ex
+++ web/controllers/send_controller.ex
@@ -8,20 +8,27 @@ defmodule PhxFormRelay.SendController do
   require Logger
 
   def send(conn, params) do
-    try do # Cast errors return a 400 instead of a 404, which we want in this case
-      form = Repo.get_by Form, id: params["phx_form_id"], active: true
-      case { Map.has_key?(params, form.honeypot), Map.get(params, form.honeypot, "") |> String.length } do
-        {true, 0} ->
-          Mailer.build_email_from_form_and_params(form, params)
-            |> Mailer.deliver
-        _ ->
-          updated_form = Ecto.Changeset.change form, count: form.count + 1
-          Repo.update updated_form
-          Email.changeset(%Email{}, %{content: Mailer.format_params(form, params), ip: conn.remote_ip |> Tuple.to_list |> Enum.join(".")}) |> Repo.insert!
-      end
+
+    case params["phx_form_id"] do 
+      <<_::64, ?-, _::32, ?-, _::32, ?-, _::32, ?-, _::96>> ->  
+        if form = Repo.get_by Form, id: params["phx_form_id"], active: true do
+          case { Map.has_key?(params, form.honeypot), Map.get(params, form.honeypot, "") |> String.length } do
+            {true, 0} ->
+              Mailer.build_email_from_form_and_params(form, params) |> Mailer.deliver
+            _ ->
+              Ecto.Changeset.change(form, count: form.count + 1) |> Repo.update
+              Email.changeset(%Email{}, %{content: Mailer.format_params(form, params), ip: conn.remote_ip 
+                |> Tuple.to_list 
+                |> Enum.join(".")}) 
+                |> Repo.insert!
+          end
+        end
+      _ ->
+    end
+    
+    if form do 
       redirect conn, external: form.redirect_to
-    rescue
-      e in _ -> e
+    else
       render(conn, "not_found.html", layout: {PhxFormRelay.LayoutView, "not_found.html"}, id: params["phx_form_id"])
     end
   end
